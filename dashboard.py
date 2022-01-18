dfaaa2eed09c19c83d6801fc32531a449ba07c20
Metric <48058391+Metricam@users.noreply.github.com>
2022-01-18 21:29:52 +0400

    setup configs

--- dashboard.py
+++ dashboard.py
@@ -4,6 +4,13 @@ import numpy as np
 import wbgapi as wb
 import plotly.express as px
 
+##configs
+country_list = ['ARM','AZE', 'GEO']
+year_str = 2005
+year_end = 2022
+year_step = 1
+app_title = 'Economic KPI Dashboard'
+
 data_indicators = pd.DataFrame(wb.series.info().items)
 
 ##sidebar
@@ -11,10 +18,10 @@ st.sidebar.image("https://cdn.icon-icons.com/icons2/2899/PNG/512/dashboard_icon_
 selected_indicator = st.sidebar.selectbox(label="Select an Indicator", options = data_indicators.value)
 selected_indicator_code = data_indicators[data_indicators.value==selected_indicator].id.values[0]
 ##body
-st.title('Economic Indicator Analysis Dashboard')
+st.title(app_title)
 
-data_plot = wb.data.DataFrame(selected_indicator_code, economy=['ARM','AZE', 'GEO'], time=range(2005,2022,1)).T.reset_index().dropna()
-data_plot = pd.melt(data_plot, id_vars=['index'], value_vars=['ARM','AZE', 'GEO'])
+data_plot = wb.data.DataFrame(selected_indicator_code, economy=country_list, time=range(year_str,year_end,year_step)).T.reset_index().dropna()
+data_plot = pd.melt(data_plot, id_vars=['index'], value_vars=country_list)
 data_plot.columns = ["Year","Country",selected_indicator]
 data_plot.Year = data_plot.Year.str.replace("YR","").astype(int)
 plot_object = px.line(data_plot, x="Year", y=selected_indicator,color="Country")
